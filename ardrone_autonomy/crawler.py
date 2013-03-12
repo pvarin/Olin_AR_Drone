809c187b2fadb0ea58c5a26840f313a9874709e0
Arjun Aletty <arjun.aletty@gmail.com>
2013-03-12 11:33:25 -0400

    improved crawler that actually looks for keywords

--- ardrone_autonomy/crawler.py
+++ ardrone_autonomy/crawler.py
@@ -1,5 +1,7 @@
 import os
 
+searchphrase = ["UDP", "udp", "5556"]
+
 for dirname, dirnames, filenames in os.walk('.'):
     # print path to all subdirectories first.
     for subdirname in dirnames:
@@ -7,11 +9,9 @@ for dirname, dirnames, filenames in os.walk('.'):
 
     # print path to all filenames.
     for filename in filenames:
-        print os.path.join(dirname, filename)
-
-# with open("file.txt", "r") as f:
-#     searchlines = f.readlines()
-# for i, line in enumerate(searchlines):
-#     if "searchphrase" in line: 
-#         for l in searchlines[i:i+3]: print l,
-#         print
+    	fn = os.path.join(dirname, filename)
+		with open(fn, "r") as f:
+		    searchlines = f.readlines()
+		for i, line in enumerate(searchlines):
+		    if searchphrase[0] or searchphrase[1] or searchphrase[2] in line: 
+		        print "%s [%d] \n" % (fn, i)
