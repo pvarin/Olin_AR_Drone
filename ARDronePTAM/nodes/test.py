4f1124d107b962da7c46a11bb4705006dfdf8e73
Eric Westman <eric@eric.(none)>
2013-04-03 21:19:23 -0400

    Updated test.py

--- ARDronePTAM/nodes/test.py
+++ ARDronePTAM/nodes/test.py
@@ -13,17 +13,22 @@ class imgEcho:
 	def __init__(self):
 		self.imgPublisher = rospy.Publisher( "camera/image_raw", Image )
 		self.imgSubscriber = rospy.Subscriber( "ardrone/front/image_raw", Image, self.echoImageCallback )
-		#cv.NamedWindow("Image window", 1)
+		cv.NamedWindow("Image window", 1)
 		self.bridge = CvBridge()
 
 	def echoImageCallback(self,data):
+
+
 		try:
 			cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
 		except CvBridgeError, e:
 			raise e
 
-		#cv.ShowImage("Image window", cv_image)
-		#cv.WaitKey(3)
+		cv.ShowImage("Image window", cv_image)
+		cv.WaitKey(3)
+
+		#data.encoding = "MONO8"
+		#self.imgPublisher.publish(data)
 
 		try:
 			self.imgPublisher.publish(self.bridge.cv_to_imgmsg(cv_image, "mono8"))
