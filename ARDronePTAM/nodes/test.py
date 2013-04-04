4ad8b5fd9e51ceb1c4b95b80bcbfc2b70b926fbf
Eric Westman <eric@eric.(none)>
2013-04-03 20:25:24 -0400

    Calibrated for AR.Drone 2.0 forward facing camera. Updated values now in PtamFixParams.yaml

--- ARDronePTAM/nodes/test.py
+++ ARDronePTAM/nodes/test.py
@@ -1,24 +1,43 @@
 #!/usr/bin/env python
-
 import roslib
 roslib.load_manifest('ARDronePTAM')
-
+import sys
 import rospy
+import cv
+from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
 
-class imgEcho(object):
+class imgEcho:
+
 	def __init__(self):
-		#self.imgPublisher = rospy.Publisher( "camera/image_raw", Image )
-		#self.imgSubscriber = rospy.Subscriber( "ardrone/front/image_raw", Image, self.echoImageCallback )
-		pass
-
+		self.imgPublisher = rospy.Publisher( "camera/image_raw", Image )
+		self.imgSubscriber = rospy.Subscriber( "ardrone/front/image_raw", Image, self.echoImageCallback )
+		#cv.NamedWindow("Image window", 1)
+		self.bridge = CvBridge()
 
 	def echoImageCallback(self,data):
-		#self.imgPublisher.publish(data)
-		pass
+		try:
+			cv_image = self.bridge.imgmsg_to_cv(data, "mono8")
+		except CvBridgeError, e:
+			raise e
 
-if __name__ == "__main__":
-	rospy.init_node('test')
-	imgEchoer = imgEcho()
+		#cv.ShowImage("Image window", cv_image)
+		#cv.WaitKey(3)
 
-	rospy.spin()
+		try:
+			self.imgPublisher.publish(self.bridge.cv_to_imgmsg(cv_image, "mono8"))
+		except CvBridgeError, e:
+			print e
+
+def main(args):
+  ie = imgEcho()
+  rospy.init_node('test')
+  try:
+    rospy.spin()
+  except KeyboardInterrupt:
+    print "Shutting down"
+  cv.DestroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
